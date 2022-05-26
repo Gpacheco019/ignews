358957b4cb24596f6ca685e2d8c213fd543a181e
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2022-05-26 13:11:44 -0300

    feat(home): created page home with styles like a figma example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-      <title>Inicio | ig.news</title>
+      <title>Home | ig.news</title>
       </Head>
-      <div>      
-        Hello World
-      </div>    
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 hey, welcome</span>
+          <h1>News about the <span>React</span> World.</h1>
+          <p>
+            get access to all the pubications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
