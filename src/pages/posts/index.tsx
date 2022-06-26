44dbb1e6ed374392ff949ffd9baf2646ee8db34e
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2022-06-26 13:36:33 -0300

    feat: add styles page single post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,9 @@
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 import Head from 'next/head';
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
-import Prismic from '@prismicio/client'
+import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
 type Post = {
@@ -26,11 +27,13 @@ export default function Posts({posts}: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href='#'>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-          </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -48,7 +51,7 @@ export const getStaticProps: GetStaticProps =  async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2));
+  // console.log(JSON.stringify(response, null, 2));
 
   const posts = response.results.map(post => {
     return {
